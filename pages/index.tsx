7d6d03b25bdf3c7db2c1f231a01bd67587805b5c
Jovan <mariothrowsfieball@gmail.com>
2022-06-30 08:42:45 +0200

    prettiefied code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,96 +1,98 @@
 // @ts-nocheck
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import {useState, useEffect} from "react"
-import ToDo from "../components/ToDo"
+import type { NextPage } from "next";
+import { useState, useEffect } from "react";
+import ToDo from "../components/ToDo";
 
-const Home: NextPage = ({recived_todos, token}) => {
-  let [to_dos, setToDos] = useState([])
-  let [to_do, setToDo] = useState("")
-   
-  useEffect(()=>{
-  if(token!==undefined) {
-    if(to_dos.length===0) setToDos(recived_todos)
-    let response = JSON.stringify({token,to_dos})
-    fetch(`${process.env.URL}/api/update`,{
-      method: "POST",
-      body: response,
-      headers: {
-        'Content-Type': "application/json",
-      },
+const Home: NextPage = ({ recived_todos, token }) => {
+  let [to_dos, setToDos] = useState([]);
+  let [to_do, setToDo] = useState("");
 
-    })
-    .then(res=>res.json())
-    .then(err=>console.log(err))
-
-  }
-  },[to_dos,token,recived_todos])
+  useEffect(() => {
+    if (token !== undefined) {
+      if (to_dos.length === 0) setToDos(recived_todos);
+      let response = JSON.stringify({ token, to_dos });
+      fetch(`${process.env.URL}/api/update`, {
+        method: "POST",
+        body: response,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((err) => console.log(err));
+    }
+  }, [to_dos, token, recived_todos]);
 
   function addElement(event: React.MouseEvent<HTMLElement>) {
-    event.preventDefault()
-    setToDos(oldToDos=>[
-    ...oldToDos,
-    to_do,
-    ])
+    event.preventDefault();
+    setToDos((oldToDos) => [...oldToDos, to_do]);
   }
 
   function removeTodo(value: string) {
-    setToDos(todos=>todos.filter(todo=>todo!==value))
+    setToDos((todos) => todos.filter((todo) => todo !== value));
   }
 
   function addTodo(e) {
-    if(e.target?.value.length>30) return
-    setToDo(e.target.value)
+    if (e.target?.value.length > 30) return;
+    setToDo(e.target.value);
   }
-  let to_do_jsx = to_dos.map((value: string, key: number)=> <ToDo key={key}  remove={()=>removeTodo(value)} value={value} />)
+  let to_do_jsx = to_dos.map((value: string, key: number) => (
+    <ToDo key={key} remove={() => removeTodo(value)} value={value} />
+  ));
   return (
     <div>
       <div className="container">
         <div className="content">
-          <div className="content--title">
-            TODO LIST
-          </div>
+          <div className="content--title">TODO LIST</div>
           <div className="content--elements">
             <form className="form">
-              <input  type="text" name="todo" onChange={addTodo} value={to_do} className="form--element form--input"/>
-              <button type="submit" name="submit" className="form--element form--submit" onClick={addElement}>SUBMIT</button>
+              <input
+                type="text"
+                name="todo"
+                onChange={addTodo}
+                value={to_do}
+                className="form--element form--input"
+              />
+              <button
+                type="submit"
+                name="submit"
+                className="form--element form--submit"
+                onClick={addElement}
+              >
+                SUBMIT
+              </button>
             </form>
-              {to_do_jsx}
+            {to_do_jsx}
           </div>
         </div>
       </div>
-    </div>  
-  )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
 
-export async function getServerSideProps(context: any){
+export async function getServerSideProps(context: any) {
   try {
-  let cookie: string = context.req.headers.cookie
-  if(cookie!==undefined){
-    let token = cookie.split("=")[1]
-    let res = await fetch(`${process.env.URL}/api/validate_token`,{
-      method: "POST",
-      body: token,
-    })
-    .catch(err=>console.log(err))
-    let data = await res.json()
-    data = JSON.parse(data.name)
-    let obj: {
-      recived_todos: string[],
-      token: string
-    } = {recived_todos: data.to_dos, token} 
-    return { props: obj }
-  }
-  }
-  catch(err) {
-  console.log(err)
+    let cookie: string = context.req.headers.cookie;
+    if (cookie !== undefined) {
+      let token = cookie.split("=")[1];
+      let res = await fetch(`${process.env.URL}/api/validate_token`, {
+        method: "POST",
+        body: token,
+      }).catch((err) => console.log(err));
+      let data = await res.json();
+      data = JSON.parse(data.name);
+      let obj: {
+        recived_todos: string[];
+        token: string;
+      } = { recived_todos: data.to_dos, token };
+      return { props: obj };
+    }
+  } catch (err) {
+    console.log(err);
   }
   return {
-    props: {}
-  }
+    props: {},
+  };
 }
-
