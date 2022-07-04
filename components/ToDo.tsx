9db58194a90214524376a75c69968f91d972599a
Jovan <mariothrowsfieball@gmail.com>
2022-07-05 00:44:08 +0200

    added ability to edit to_dos

--- components/ToDo.tsx
+++ components/ToDo.tsx
@@ -1,11 +1,49 @@
+import {useState, useEffect} from "react"
+
 export default function ToDo(props: any) {
+  const [editing, setEditing] = useState(false)
+  const [toDo, setToDo] = useState(props.value)
+
+  useEffect(()=>{
+  setToDo(props.value)
+  },[props.value])
+
   function remove() {
     props.remove();
     console.log("hello");
   }
+
+  function edit() {
+  setEditing(true)
+  }
+
+  function update(e) {
+  setToDo(e.target.value)
+  }
+
+  function done() {
+    props.setToDos((oldToDos: string[])=>{
+    oldToDos[props.index] = toDo
+    return [...oldToDos]
+  })
+  setEditing(false)
+  }
+
+
+  if(!editing){
   return (
     <div className="todo">
-      <span onClick={remove}>🗑️</span> {props.value}
+      <span><span onClick={remove}>🗑️</span> {props.value}</span>
+      <button onClick={edit}>Edit</button>
     </div>
   );
+  }
+  else {
+    return (
+    <div className="todo">
+    <input onChange={update} value={toDo}></input>
+    <button onClick={done}>Done</button>
+    </div>
+    )
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,23 @@
 import type { NextPage } from "next";
 import { useState, useEffect } from "react";
 import ToDo from "../components/ToDo";
-import { getCookie } from 'cookies-next'
-
-
+import { getCookie } from "cookies-next";
 
 const Home: NextPage = ({ recived_todos, token, setUsername, username }) => {
   let [to_dos, setToDos] = useState([]);
   let [to_do, setToDo] = useState("");
 
   useEffect(() => {
-    if (getCookie("token")!==undefined) {
-      if (to_dos.length === 0)  setToDos(recived_todos);
+    if (getCookie("token") === undefined) {
+      if (localStorage.getItem("to_dos"))
+        setToDos(JSON.parse(localStorage.getItem("to_dos")));
+      else setToDos([]);
+    }
+  }, [getCookie("token")]);
+
+  useEffect(() => {
+    if (getCookie("token") !== undefined) {
+      if (to_dos.length === 0) setToDos(recived_todos);
       let response = JSON.stringify({ token, to_dos });
       fetch(`${process.env.URL}/api/update`, {
         method: "POST",
@@ -22,41 +28,39 @@ const Home: NextPage = ({ recived_todos, token, setUsername, username }) => {
         },
       })
         .then((res) => res.json())
-        .then(data=>{
-          setUsername(username)
+        .then((data) => {
+          setUsername(username);
         })
         .catch((err) => console.log(err));
+    } else {
+      localStorage.setItem("to_dos", JSON.stringify(to_dos));
+      update_to_dos_if_local();
     }
   }, [to_dos, token, recived_todos]);
 
-   
-  useEffect(()=>{
-   if(getCookie("token")===undefined) {
-     if(localStorage.getItem("to_dos")) setToDos(JSON.parse(localStorage.getItem("to_dos")))
-     else setToDos([])
-   }
-  },[getCookie("token")])
-   
-  useEffect(()=>{
-    if(to_dos.length === 0&&getCookie("token")===undefined) {
-    let to_dos_from_local_storage = localStorage.getItem("to_dos")
-    if(to_dos_from_local_storage!==undefined) {
-      setToDos(JSON.parse(to_dos_from_local_storage))
-    }
-    }
-  },[])
+  function update_to_dos_if_local() {
+    let to_dos_from_local_storage = localStorage.getItem("to_dos");
+      setToDos((oldToDos) => {
+        let newToDos = JSON.parse(to_dos_from_local_storage);
+        if (oldToDos.toString() !== newToDos.toString()) return oldToDos;
+        else return oldToDos;
+      });
+  }
 
   function addElement(event: React.MouseEvent<HTMLElement>) {
     event.preventDefault();
     setToDos((oldToDos) => {
-    let new_to_dos =  [...oldToDos, to_do]
-    if(getCookie("token")===undefined) localStorage.setItem("to_dos", JSON.stringify(new_to_dos))
-    return new_to_dos
+      let new_to_dos = [...oldToDos, to_do];
+      if (getCookie("token") === undefined)
+        localStorage.setItem("to_dos", JSON.stringify(new_to_dos));
+      return new_to_dos;
     });
-    setToDo("")
+    setToDo("");
   }
 
   function removeTodo(value: string) {
+    to_do_jsx = generate_to_do_jsx();
+    console.log(to_do_jsx);
     setToDos((todos) => todos.filter((todo) => todo !== value));
   }
 
@@ -64,9 +68,20 @@ const Home: NextPage = ({ recived_todos, token, setUsername, username }) => {
     if (e.target?.value.length > 30) return;
     setToDo(e.target.value);
   }
-  let to_do_jsx = to_dos.map((value: string, key: number) => (
-    <ToDo key={key} remove={() => removeTodo(value)} value={value} />
-  ));
+
+  let to_do_jsx = generate_to_do_jsx();
+
+  function generate_to_do_jsx() {
+    return to_dos.map((value: string, key: number) => (
+      <ToDo
+        key={key}
+        setToDos={setToDos}
+        index={key}
+        remove={() => removeTodo(value)}
+        value={value}
+      />
+    ));
+  }
 
   return (
     <div>
@@ -113,12 +128,12 @@ export async function getServerSideProps(context: any) {
       }).catch((err) => console.log(err));
       let data = await res.json();
       data = JSON.parse(data.name);
-      console.log(data,"hi")
+      console.log(data, "hi");
       let obj: {
         recived_todos: string[];
         token: string;
-        username: string
-      } = { recived_todos: data.to_dos, token, username: data.username};
+        username: string;
+      } = { recived_todos: data.to_dos, token, username: data.username };
       return { props: obj };
     }
   } catch (err) {
